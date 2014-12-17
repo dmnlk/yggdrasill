50c684937faece0facbdd2c54ad1ab3cdc463d43
dmnlk <seikima2demon@gmail.com>
2014-12-18 01:18:12 +0900

    get KEY from env var

--- src/yggdrasill/yggdrasill.go
+++ src/yggdrasill/yggdrasill.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
+var (
+	CONSUMER_KEY string
+	CONSUMER_KEY_SECRET string
+	ACCESS_TOKEN string
+	ACCESS_TOKEN_SECRET string
+)
 func main() {
-	fmt.Printf("Hello world!")
+
+	_ = configureToken()
+
+	fmt.Println(CONSUMER_KEY)
+	fmt.Println(CONSUMER_KEY_SECRET)
+	fmt.Println(ACCESS_TOKEN)
+	fmt.Println(ACCESS_TOKEN_SECRET)
+}
+
+func  configureToken()(error) {
+	CONSUMER_KEY = os.Getenv("CONSUMER_KEY")
+	CONSUMER_KEY_SECRET = os.Getenv("CONSUMER_KEY_SECRET")
+	ACCESS_TOKEN = os.Getenv("ACCESS_TOKEN")
+	ACCESS_TOKEN_SECRET = os.Getenv("ACCESS_TOKEN_SECRET")
+
+	if CONSUMER_KEY != "" || CONSUMER_KEY_SECRET == "" || ACCESS_TOKEN == "" || ACCESS_TOKEN_SECRET == "" {
+		return  fmt.Errorf("error %d", 1)
+	}
+	return nil
 }
